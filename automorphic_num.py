0e46e34d0963d89d41ee7120f8f4f7235a9bcb36
mohammad sameer <sameermohammad1107@gmail.com>
2025-07-07 16:59:12 +0530

    Delete automorphic_num.py

--- automorphic_num.py
+++
@@ -1,9 +0,0 @@
-# Automarphic number
-'''After squaring the given number if the last digits of the result is equal to input number then the number is automorphic number'''
-n=int(input("enter a number : "))
-power=n**2
-print("power of the number is :",power)
-if n%100 == n:
-    print("number is automorphic")
-else:
-    print("number is not automarphic")
