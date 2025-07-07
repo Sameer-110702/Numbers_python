d406849f161e70dc2608b00dab57744bf4ca8cc0
mohammad sameer <sameermohammad1107@gmail.com>
2025-07-07 19:50:05 +0530

    Update alternate_prime_num.py

--- alternate_prime_num.py
+++ alternate_prime_num.py
@@ -5,7 +5,7 @@ for i in range(1,n+1):
     for j in range(1,i+1):
         if i%j==0:
             count=count+1
-    if count==2:
+    if count<=2:
         primes.append(i)
 print("Prime numbers in a given range : ",primes)
 print("Alternate prime numbers in the given range are : ",primes[0:n+1:2])
